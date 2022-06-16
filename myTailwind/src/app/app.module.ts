915ee5402125532e65bf581ff275351a8b03acf0
bishnu <bishnu.gurung@selise.ch>
2022-06-16 19:50:12 +0600

    new feature vacancy

--- myTailwind/src/app/app.module.ts
+++ myTailwind/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreatevacancyComponent } from './createvacancy/createvacancy.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreatevacancyComponent
   ],
   imports: [
     BrowserModule,
---
+++ myTailwind/src/app/createvacancy/createvacancy.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-createvacancy',
+  templateUrl: './createvacancy.component.html',
+  styleUrls: ['./createvacancy.component.css']
+})
+export class CreatevacancyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
