02c8b3bc122d84044fb6f640999cc3d27b1d8f89
bishnu <bishnu.gurung@selise.ch>
2022-06-20 13:32:07 +0600

    create vacancy listing

---
+++ myTailwind/src/app/vacancylist/vacancylist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vacancylist',
+  templateUrl: './vacancylist.component.html',
+  styleUrls: ['./vacancylist.component.css']
+})
+export class VacancylistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
